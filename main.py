5d796aab0b521f7363a354fc989b6f80c49c684f
Hawre9193 <tvf4019@gmail.com>
2026-07-18 14:38:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,22 @@ if st.button("ئەنجام"):
     else:
         st.write("ئەنجام:", num1 - num2)
       
+###############################################
+import streamlit as st
+
+st.title("حاسیبەی پێشکەوتوو")
+
+num1 = st.number_input("ژمارەی یەکەم")
+num2 = st.number_input("ژمارەی دووەم")
+
+# لێرەدا هەموو کردارەکان دەخەینە ناو لیستێکەوە
+operations = ["کۆکردنەوە", "لێدەرکردن", "لێکدان", "دابەشکردن"]
+op = st.selectbox("جۆری کردار", operations)
+
+# لێرەدا پایتۆن خۆی بەپێی هەڵبژاردنەکەی تۆ کردارەکە دەکات
+if op == "کۆکردنەوە": st.write("ئەنجام:", num1 + num2)
+elif op == "لێدەرکردن": st.write("ئەنجام:", num1 - num2)
+elif op == "لێکدان": st.write("ئەنجام:", num1 * num2)
+elif op == "دابەشکردن":
+    if num2 != 0: st.write("ئەنجام:", num1 / num2)
+    else: st.write("هەڵە: دابەشکردن بەسەر سفر نابێت")
