ee7ef201328257e5e2abe456280fcebfea125d81
Hawre9193 <tvf4019@gmail.com>
2026-07-18 14:42:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
 import streamlit as st
 
-st.title("حاسیبەی سادە")
-
-num1 = st.number_input("ژمارەی یەکەم")
-num2 = st.number_input("ژمارەی دووەم")
-operation = st.selectbox("جۆری کردار", ["کۆکردنەوە", "لێدەرکردن"])
-
-if st.button("ئەنجام"):
-    if operation == "کۆکردنەوە":
-        st.write("ئەنجام:", num1 + num2)
-    else:
-        st.write("ئەنجام:", num1 - num2)
-      
-###############################################
-import streamlit as st
-
 st.title("حاسیبەی پێشکەوتوو")
 
 num1 = st.number_input("ژمارەی یەکەم")
